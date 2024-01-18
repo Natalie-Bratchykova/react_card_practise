4547dc7ebd37162d9a862fe45c045b930d53a123
Natalie-Bratchykova <nataliya.bratchykova@gmail.com>
2024-01-18 20:26:40 +0200

    created function card() with props params

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// function for loader
+function Loader(){
+  
+}
+
+// function for cards
+function Card(props){
+  return(
+    <div className="card">
+      <h2 className="card__title">{props.title}</h2>
+      <img src={props.src} alt={props.title}></img>
+    </div>
+  );
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My react app</h1>
+      <Card src="https://www.hindustantimes.com/ht-img/img/2023/10/30/550x309/Toge_Inumaki_1698679014859_1698679033339.jfif" title="Inumaki Toge"/>
+      <Card title ="Kugisaki Nobara" src="https://staticg.sportskeeda.com/editor/2022/10/53080-16654550806994-1920.jpg?w=840"/>
+      <Card title="Itsuomi Nagi" src="https://pbs.twimg.com/media/EQ3iTieUwAAtm_h.jpg"/>
     </div>
   );
 }
